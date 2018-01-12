8bf8721f11a56a698ada75563c18eaca0d6f48a4
Martin Pugh <mpugh@athenahealth.com>
2018-01-11 22:27:50 -0500

    New ValidateBBSession just checks that the BBSession global variable exists

---
+++ Private/ValidateBBSession.ps1
@@ -0,0 +1,12 @@
+﻿Function ValidateBBSession
+{
+    [CmdletBinding()]
+    Param ()
+
+    Try {
+        $null = Get-Variable -Name BBSession
+    }
+    Catch {
+        Write-Error "You are not currently connected to a BitBucket server.  Run New-BBSession." -ErrorAction Stop
+    }
+}
