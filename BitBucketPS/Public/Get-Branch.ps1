2aff5767f07357ed71de14b4beac14b50c892eac
Martin Pugh <mpugh@athenahealth.com>
2018-01-14 14:41:54 -0500

    Added comment based help to Get-Branch

--- BitBucketPS/Public/Get-Branch.ps1
+++ BitBucketPS/Public/Get-Branch.ps1
@@ -1,4 +1,39 @@
 Function Get-Branch {
+    <#
+    .SYNOPSIS
+        Get branch information from a designated repository
+    .PARAMETER Repo
+        Name or partial name of the repository you want to query
+    .PARAMETER Branch
+        Name or partial name of the branch you want to query
+    .PARAMETER ExcludePersonalProjects
+        If multiple forks of a repository exist, Get-Branch will show them all.  Use this parameter to exclude those.
+    .PARAMETER Project
+        If multiple forks of a repository exist you can use this filter to limit which projects you want to see.
+    .INPUTS
+        None
+    .OUTPUTS
+        [PSCustomObject]
+    .EXAMPLE
+        Get-Branch -Repo BitbucketPS
+
+        This will show all of the branchs, from all forks of any repository with that matches BitbucketPS.
+
+    .EXAMPLE
+        Get-Branch -Repo BitbucketPS -Branch dev
+
+        Same as first example, but will limit output to branches that match dev
+    .EXAMPLE
+        Get-Branch -Repo BitbucketPS -ExcludePersonalProjects
+
+        This will limit the output to branches that are not in peoples personal projects.
+    .EXAMPLE
+        Get-Branch -Repo BitbucketPS -Project ~
+
+        This would limit the output to only personal projects.
+    .NOTES
+    .LINK
+    #>
     [CmdletBinding(DefaultParameterSetName="All")]
     Param (
         [Parameter(Mandatory)]
